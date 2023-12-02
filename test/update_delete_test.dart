93944388d273d662740ad01cbd586c399426ed6e
babibulat <junetajulia0407@gmail.com>
2023-12-02 23:08:39 +0700

    update delete 2

--- test/update_delete_test.dart
+++ test/update_delete_test.dart
@@ -2,51 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test_slicing/presentations/screens/ulasan/tambah_ulasan_screen.dart';
-import 'package:test_slicing/data/model/destinasi.dart'; // Updated import
+import 'package:test_slicing/data/model/destinasi.dart';
+import 'package:test_slicing/presentations/screens/ulasan/ulasan_screen.dart';
 
 void main() {
-  testWidgets('Test Update Ulasan', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Material(
-        child: TambahUlasanScreen(),
-      ),
-    ));
 
-    await tester.pumpAndSettle();
+    testWidgets('Test Update Ulasan', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Material(
+          child: TambahUlasanScreen(),
+        ),
+      ));
 
-    // Find the review text field
-    final ratingBar = find.byType(RatingBar);
-    final reviewField = find.widgetWithText(TextFormField, 'Review :');
-    final postButton = find.byType(ElevatedButton);
+      await tester.pumpAndSettle();
+
+      final ratingBar = find.byType(RatingBar);
+      final reviewField = find.widgetWithText(TextField, 'Review :');
+      final postButton = find.byType(ElevatedButton);
+      
+      expect(ratingBar, findsOneWidget);
+      expect(reviewField, findsOneWidget);
+      expect(postButton, findsOneWidget);
+
+      await tester.tap(ratingBar);
+      await tester.pumpAndSettle();
+
+      await tester.tap(postButton);
+      await tester.pumpAndSettle();
+
+      await tester.enterText(reviewField, 'Updated Review');
+    });
     
-    expect(ratingBar, findsOneWidget);
-    expect(reviewField, findsOneWidget);
-    expect(postButton, findsOneWidget);
+    testWidgets('Test Delete Ulasan', (WidgetTester tester) async {
+  
+    final ulasanData = Ulasan(
+      idPengguna: 'user123',
+      rating: '4',
+      ulasan: 'Ini adalah ulasan pengguna',
+    );
 
-    await tester.tap(ratingBar);
-    await tester.pumpAndSettle();
-
-    await tester.tap(postButton);
-    await tester.pumpAndSettle();
-
-    await tester.enterText(reviewField, 'Updated Review');
-  });
-
-  testWidgets('Test Delete Ulasan', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
-      home: Material(
-        child: TambahUlasanScreen(),
+      home: UlasanVerticalCard(
+        size: Size(300, 200), 
+        ulasan: ulasanData,
       ),
     ));
 
     await tester.pumpAndSettle();
 
-    final deleteButton = find.byType(TextButton); // Replace FlatButton with your delete button type
-    expect(deleteButton, findsOneWidget);
+    final deleteButtonFinder = find.widgetWithText(TextButton, 'Delete');
+    expect(deleteButtonFinder, findsOneWidget);
 
-    await tester.tap(deleteButton);
-    await tester.pumpAndSettle();
-
-    expect(find.text('Delete Review'), findsNothing);
+    final deleteButton = tester.widget<TextButton>(deleteButtonFinder);
+    deleteButton.onPressed!(); 
   });
+
+
+
+
+
 }
